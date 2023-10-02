86aca3eb11d97ca19f8dbcd4e4b0d77a84b3717b
MuhammadJalilAsyakur <jalilasyakur@gmail.com>
2023-10-02 21:07:44 +0700

    initial commit

---
+++ app.js
@@ -0,0 +1,186 @@
+let digimon = [
+    {
+        "name": "Koromon",
+        "img": "https://digimon.shadowsmith.com/img/koromon.jpg",
+        "level": "In Training"
+    },
+    {
+        "name": "Tsunomon",
+        "img": "https://digimon.shadowsmith.com/img/tsunomon.jpg",
+        "level": "In Training"
+    },
+    {
+        "name": "Yokomon",
+        "img": "https://digimon.shadowsmith.com/img/yokomon.jpg",
+        "level": "In Training"
+    },
+    {
+        "name": "Motimon",
+        "img": "https://digimon.shadowsmith.com/img/motimon.jpg",
+        "level": "In Training"
+    },
+    {
+        "name": "Tanemon",
+        "img": "https://digimon.shadowsmith.com/img/tanemon.jpg",
+        "level": "In Training"
+    },
+    {
+        "name": "Bukamon",
+        "img": "https://digimon.shadowsmith.com/img/bukamon.jpg",
+        "level": "In Training"
+    },
+    {
+        "name": "Tokomon",
+        "img": "https://digimon.shadowsmith.com/img/tokomon.jpg",
+        "level": "In Training"
+    },
+    {
+        "name": "Agumon",
+        "img": "https://digimon.shadowsmith.com/img/agumon.jpg",
+        "level": "Rookie"
+    },
+    {
+        "name": "Gabumon",
+        "img": "https://digimon.shadowsmith.com/img/gabumon.jpg",
+        "level": "Rookie"
+    },
+    {
+        "name": "Biyomon",
+        "img": "https://digimon.shadowsmith.com/img/biyomon.jpg",
+        "level": "Rookie"
+    },
+    {
+        "name": "Tentomon",
+        "img": "https://digimon.shadowsmith.com/img/tentomon.jpg",
+        "level": "Rookie"
+    },
+    {
+        "name": "Palmon",
+        "img": "https://digimon.shadowsmith.com/img/palmon.jpg",
+        "level": "Rookie"
+    },
+    {
+        "name": "Gomamon",
+        "img": "https://digimon.shadowsmith.com/img/gomamon.jpg",
+        "level": "Rookie"
+    },
+    {
+        "name": "Patamon",
+        "img": "https://digimon.shadowsmith.com/img/patamon.jpg",
+        "level": "Rookie"
+    },
+    {
+        "name": "Kuwagamon",
+        "img": "https://digimon.shadowsmith.com/img/kuwagamon.jpg",
+        "level": "Champion"
+    },
+    {
+        "name": "Greymon",
+        "img": "https://digimon.shadowsmith.com/img/greymon.jpg",
+        "level": "Champion"
+    },
+    {
+        "name": "Shellmon",
+        "img": "https://digimon.shadowsmith.com/img/shellmon.jpg",
+        "level": "Champion"
+    },
+    {
+        "name": "Garurumon",
+        "img": "https://digimon.shadowsmith.com/img/garurumon.jpg",
+        "level": "Champion"
+    },
+    {
+        "name": "Seadramon",
+        "img": "https://digimon.shadowsmith.com/img/seadramon.jpg",
+        "level": "Champion"
+    },
+    {
+        "name": "Monochromon",
+        "img": "https://digimon.shadowsmith.com/img/monochromon.jpg",
+        "level": "Champion"
+    },
+    {
+        "name": "Birdramon",
+        "img": "https://digimon.shadowsmith.com/img/birdramon.jpg",
+        "level": "Champion"
+    },
+    {
+        "name": "Meramon",
+        "img": "https://digimon.shadowsmith.com/img/meramon.jpg",
+        "level": "Champion"
+    },
+    {
+        "name": "Kabuterimon",
+        "img": "https://digimon.shadowsmith.com/img/kabuterimon.jpg",
+        "level": "Champion"
+    },
+    {
+        "name": "Andromon",
+        "img": "https://digimon.shadowsmith.com/img/andromon.jpg",
+        "level": "Ultimate"
+    },
+    {
+        "name": "Togemon",
+        "img": "https://digimon.shadowsmith.com/img/togemon.jpg",
+        "level": "Champion"
+    },
+    {
+        "name": "Numemon",
+        "img": "https://digimon.shadowsmith.com/img/numemon.jpg",
+        "level": "Champion"
+    },
+    {
+        "name": "Monzaemon",
+        "img": "https://digimon.shadowsmith.com/img/monzaemon.jpg",
+        "level": "Ultimate"
+    },
+    {
+        "name": "Ikkakumon",
+        "img": "https://digimon.shadowsmith.com/img/ikkakumon.jpg",
+        "level": "Champion"
+    },
+]
+
+
+// mandapatkan element html dengan id digimon
+// let listDigimon = document.getElementById("digimon");
+
+
+// menggunakan for loop untuk mendapatkan array of object
+
+// for (let i = 0; i < digimon.length; i++) {
+//     let cardDigimon = `
+//         <div class="card">
+//             <img src= ${digimon[i].img} />
+//             <h4>${digimon[i].name}</h4>
+//             <p>${digimon[i].level}</p>
+//         </div>`;
+
+//     listDigimon.innerHTML += cardDigimon;
+// }
+
+
+
+
+
+// menggunakan reusable function map dan filter untuk mendapatkan array of object
+const digiTrain = digimon.filter((digiTrain) => digiTrain.level === "In Training").map((digiTrain) => {
+    let cardDigimon = `
+    <div class="card">
+        <img src= ${digiTrain.img} />
+        <h4>${digiTrain.name}</h4>
+        <p>${digiTrain.level}</p>
+    </div>`;
+
+    listDigimon.innerHTML += cardDigimon;
+})
+
+// function searchPokemonByName(name, digimons) {
+//     return digimons.filter(digimon => digimon.name.toLowerCase().includes(name.toLowerCase()));
+// }
+
+// const searchResults = searchPokemonByName("Koromon", digimon);
+// console.log(searchResults);
+
+
+
